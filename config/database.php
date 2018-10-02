9735dca5caeb2ba37253bb99d1b851d8f96a4a99
Daniela M. santos <Daniela M. santos>
2018-10-02 17:21:14 +0100

    update for project

--- config/database.php
+++ config/database.php
@@ -41,6 +41,20 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 'root'),
+            'database' => env('DB_DATABASE', 'project01'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+       /* 'mysql' => [
+            'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -52,7 +66,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
+        ],*/
 
         'pgsql' => [
             'driver' => 'pgsql',
---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('welcome')
+@section('container')
+    <div class="row justify-content-start">
+        <div class="col-4">
+            Será
+        </div>
+        <div class="col-8">
+            Teste
+        </div>
+</div>
+@stop
